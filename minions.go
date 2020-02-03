e9f5a4264a92a926e6dec6f2ec4fa779971aee7a
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-02-03 11:23:51 -0800

    init minions dna

---
+++ minions.go
@@ -0,0 +1,48 @@
+package minions
+
+import (
+	"sync"
+)
+
+type Command struct {
+	Instructions func()
+}
+
+type Pool struct {
+	queue chan Command
+	wg    sync.WaitGroup
+}
+
+func birthMinion(q chan Command, wg sync.WaitGroup) {
+	for j := range q {
+		j.Instructions()
+	}
+	wg.Done()
+}
+
+// Create a pool with the given number of minions
+func Hire(nbMinions int) *Pool {
+	var wg sync.WaitGroup
+	q := make(chan Command, nbMinions)
+
+	for i := 0; i < nbMinions; i++ {
+		wg.Add(1)
+		go birthMinion(q, wg)
+	}
+
+	return &Pool{
+		queue: q,
+		wg:    wg,
+	}
+}
+
+// Push a command with some instructions to the pool
+func (p *Pool) Execute(c Command) {
+	p.queue <- c
+}
+
+// Decommission the minions and wait for them to finish their work
+func (p *Pool) Terminate() {
+	close(p.queue)
+	p.wg.Wait()
+}
